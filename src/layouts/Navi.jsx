f9d28cc2e9523eb9d9385f9955bce5bdaaa87723
Ali Alpay <alialpay96@hotmail.com>
2021-06-20 00:51:56 +0300

    onClick

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CartSummary from './CartSummary'
 import {Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+    function handleSignOut(params) {
+        setIsAuthenticated(false)
+    }
+    function handleSignIn(params) {
+        setIsAuthenticated(true)
+    }
     return (
         <div>
             <Menu inverted fixed ="top">
                 <Container>
                     <Menu.Item name='home'/>
                     <Menu.Item name='messages'/>
+
                     <Menu.Menu position='right'>
                        <CartSummary/>
-                        <SignedOut/>
-                        <SignedIn/>
+                       {isAuthenticated?<SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
                     </Menu.Menu>
                 </Container>
             </Menu>
--- src/layouts/SignedOut.jsx
+++ src/layouts/SignedOut.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button, Menu } from 'semantic-ui-react'
 
-export default function SignedOut() {
+export default function SignedOut(props) {
     return (
         <div>
             <Menu.Item>
-            <Button primary>Giriş yap</Button>
+            <Button primary onClick={props.signIn}>Giriş yap</Button>
             <Button primary style={{marginLeft:"0.5em"}}>Kayıt ol</Button>
             </Menu.Item>
         </div>
