ebf43ef43692f09aecb418206a39bb0ad895140f
Ali Alpay <alialpay96@hotmail.com>
2021-06-20 00:25:22 +0300

    signIn-signOut

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Dropdown, Menu, Image } from 'semantic-ui-react'
+
+export default function SignedIn() {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://avatars.githubusercontent.com/u/63648362?s=60&u=0212522ebaaab294f780cc30ba80f51d4c607710&v=4"/>
+                <Dropdown pointing="top left" text="Ali">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item text="Çıkış yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
