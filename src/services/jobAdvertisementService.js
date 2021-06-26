530d68cd4dd0e78e0155d1930c0fdb7505858582
Ali Alpay <alialpay96@hotmail.com>
2021-06-27 00:24:04 +0300

    jobAdvertisementAdd going to be fix

--- src/services/jobAdvertisementService.js
+++ src/services/jobAdvertisementService.js
@@ -9,4 +9,8 @@ export default class JobAdvertisementService{
         return axios.get("http://localhost:8080/api/jobAdvertisements/getByApplicationDeadline?date="+date)
     }
 
+    postJobAdvertisements(){
+        return axios.post("http://localhost:8080/api/jobAdvertisements/add")
+    }
+
 }
