731567c09362ed8b487eb6b65bc1e299bd8adf08
Ali Alpay <alialpay96@hotmail.com>
2021-06-26 23:22:38 +0300

    JobAdvertisementAdd_Form

--- src/pages/JobAdvertisementAdd.jsx
+++ src/pages/JobAdvertisementAdd.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import { Formik, Form, Field } from 'formik'
+import React, { useState, useEffect } from 'react'
+import { Formik, Form } from 'formik'
 import * as Yup from "yup";
-import { FormField, Button, Label } from 'semantic-ui-react';
+import { FormField, Button, Label, Dropdown, Select } from 'semantic-ui-react';
 import KodlamaIoTextInput from '../utilities/customFormControls/KodlamaIoTextInput';
+import CityService from '../services/cityService';
+
+
+
 
 export default function JobAdvertisementAdd() {
-    const initialValues = { title: ""}
+    const initialValues = { title: "" }
+
+    const workingType = [
+        { key: 1, text: "Tam Zamanlı", value: 1 },
+        { key: 2, text: "Yarı Zamanlı", value: 2 }
+    ]
+    const workingTime = [
+        { key: 1, text: "Uzaktan", value: 1 },
+        { text: "Ofis", key: 0, value: 0 }
+    ]
+    const [cities, setCities] = useState([]);
+
+    useEffect(() => {
+        let cityService = new CityService();
+        cityService.getCities().then(result => setCities(result.data.data.map((city) => {
+            return { text: city.cityName, key: city.id, value: city.id }
+        })
+        ))
+    }, [])
 
     const schema = Yup.object({
         title: Yup.string().required("Pozisyon adı zorunlu"),
@@ -14,34 +36,55 @@ export default function JobAdvertisementAdd() {
         maxSalary: Yup.number().required("Minimum maaş limiti zorunlu"),
         numberOfOpenPosition: Yup.number().required("Açık pozisyon adedi zorunlu"),
         createdAt: Yup.date().required("Yayın tarihi zorunlu"),
-        applicationDeadline: Yup.date().required("Son başvuru tarihi zorunlu"),
-        employer: Yup.string().required("Kurum adı zorunlu"),
-        city: Yup.string().required("Şehir bilgisi zorunlu")
-        
+        applicationDeadline: Yup.date().required("Son başvuru tarihi zorunlu")
+
     })
 
     return (
         <div>
-        İlan Ekleme İşlemi
-        <Formik
-            initialValues={initialValues}
-            validationSchema={schema}
-            onSubmit={(values) => {
-                console.log(values)
-            }}
-        >
-            <Form className="ui form">
-                <KodlamaIoTextInput name="title" placeholder="Pozisyon adı" />
-                <KodlamaIoTextInput name="jobDescription" placeholder="Açıklama" />
-                <KodlamaIoTextInput name="minSalary" placeholder="Min. maaş" />
-                <KodlamaIoTextInput name="maxSalary" placeholder="Max. maaş" />
-                <KodlamaIoTextInput name="numberOfOpenPosition" placeholder="Açık pozisyon adedi" />
-                <KodlamaIoTextInput name="createdAt" placeholder="Yayınlanma tarihi" />
-                <KodlamaIoTextInput name="applicationDeadline" placeholder="Son başvuru" />
-                <KodlamaIoTextInput name="city" placeholder="Şehir" />
-                <Button color="green" type="submit">Ekle</Button>
-            </Form>
-        </Formik>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={schema}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+            >
+                <Form className="ui form">
+                    <KodlamaIoTextInput name="title" placeholder="Pozisyon adı" />
+                    <KodlamaIoTextInput name="jobDescription" placeholder="Açıklama" />
+                    <KodlamaIoTextInput name="minSalary" placeholder="Min. maaş" />
+                    <KodlamaIoTextInput name="maxSalary" placeholder="Max. maaş" />
+                    <KodlamaIoTextInput name="numberOfOpenPosition" placeholder="Açık pozisyon adedi" />
+                    <KodlamaIoTextInput name="createdAt" placeholder="Yayınlanma tarihi" />
+                    <KodlamaIoTextInput name="applicationDeadline" placeholder="Son başvuru" />
+
+                    <FormField>
+                    <label>Şehirler</label>
+                        <Dropdown
+                            clearable
+                            fludi
+                            multiple
+                            search
+                            selection
+                            options={cities}
+                            placeholder="Şehir Seçiniz" />
+                    </FormField>
+
+                    <FormField>
+                    <label>Çalışma Tipi</label>
+                        <Select placeholder='Çalışma Tipi Seçiniz' options={workingTime} />
+                    </FormField>
+
+                    <FormField>
+                    <label>Çalışma Zamanı</label>
+                        <Select placeholder='Çalışma Zamanı Seçiniz' options={workingType} />
+                    </FormField>
+
+
+                    <Button color="green" type="submit">Ekle</Button>
+                </Form>
+            </Formik>
+
         </div>
     )
 }
